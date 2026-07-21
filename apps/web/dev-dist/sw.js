a2a22590494084c7be669e340b10d5eee20bc5ca
gustavonunes0 <gustavonoroes@outlook.com>
2026-07-21 18:14:07 -0300

    refactor: deploy

--- apps/web/dev-dist/sw.js
+++ apps/web/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.od7kq24qpv8"
+    "revision": "0.rl6k0tc4c3"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
