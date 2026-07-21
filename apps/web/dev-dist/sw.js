e26e70e0d64f0825793dee8ff8bfbfd7426f63bb
gustavonunes0 <gustavonoroes@outlook.com>
2026-07-21 17:49:24 -0300

    fix: build

--- apps/web/dev-dist/sw.js
+++ apps/web/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.gg9of4tj34o"
+    "revision": "0.od7kq24qpv8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
